e1076e49035d1aafa65dfba4823935362acbb795
EricWithC04 <negativegame117@gmail.com>
2025-06-03 11:37:22 -0300

    feat: cards

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,14 @@
-import { Box, List, ListItem, ListItemButton, ListItemIcon, ListItemText } from '@mui/material'
+import { Box, Button, List, ListItem, ListItemButton, ListItemIcon, ListItemText, ThemeProvider, Typography, createTheme } from '@mui/material'
+import { Card, CardMedia, CardContent, CardActions } from "@mui/material"
 import { Home, Dashboard, Settings, Person, Logout } from '@mui/icons-material'
+import CssBaseline from '@mui/material/CssBaseline'
 import './App.css'
+
+const darkTheme = createTheme({
+	palette: {
+		mode: 'dark'
+	}
+})
 
 function App() {
 
@@ -32,30 +40,78 @@ function App() {
 		}
 	]
 
+	const cards = [
+		{
+			image: "https://eq2imhfmrcc.exactdn.com/wp-content/uploads/2016/08/golden-retriever.jpg?strip=all&lossy=1&ssl=1",
+			title: "Perro",
+			description: "Esta es la descripción",
+		},
+		{
+			image: "https://eq2imhfmrcc.exactdn.com/wp-content/uploads/2016/08/golden-retriever.jpg?strip=all&lossy=1&ssl=1",
+			title: "Perro",
+			description: "Esta es la descripción",
+		},
+		{
+			image: "https://eq2imhfmrcc.exactdn.com/wp-content/uploads/2016/08/golden-retriever.jpg?strip=all&lossy=1&ssl=1",
+			title: "Perro",
+			description: "Esta es la descripción",
+		},
+	]
+
 	return (
-		<div>
-			<Box sx={{ width: 250 }}>
-				<List>
+		<ThemeProvider theme={darkTheme}>
+			<CssBaseline />
+			<Box height={"100vh"} width={"100%"} display={"flex"}>
+				<Box sx={{ width: 250 }}>
+					<List>
+						{
+							options.map((option) => (
+								<ListItem>
+									<ListItemButton>
+										<ListItemIcon>
+											<option.icon />
+										</ListItemIcon>
+										<ListItemText>
+											{option.name}
+										</ListItemText>
+									</ListItemButton>
+								</ListItem>
+							))
+						}
+					</List>
+				</Box>
+				<Box 
+					flexGrow={1} 
+					display={"grid"} 
+					gridTemplateColumns={"repeat(auto-fill, minmax(30%, 1fr))"} 
+					gridTemplateRows={"repeat(2, 1fr)"}
+					gap={"5%"}
+					marginTop={"1rem"}
+				>
 					{
-						options.map((option) => (
-							<ListItem>
-								<ListItemButton>
-									<ListItemIcon>
-										<option.icon />
-									</ListItemIcon>
-									<ListItemText>
-										{option.name}
-									</ListItemText>
-								</ListItemButton>
-							</ListItem>
+						cards.map((card) => (
+							<Card>
+								<CardMedia 
+									sx={{ height: "50%" }}
+									image={card.image}
+									title={card.title}
+									
+								/>
+								<CardContent>
+									<Typography gutterBottom variant="h5" component="div">{card.title}</Typography>
+									<Typography variant="body2" sx={{ color: "text.secondary" }}>
+										{card.description}
+									</Typography>
+								</CardContent>
+								<CardActions>
+									<Button variant={'outlined'}>Ver más</Button>
+								</CardActions>
+							</Card>
 						))
 					}
-				</List>
+				</Box>
 			</Box>
-			<Box>
-				
-			</Box>
-		</div>
+		</ThemeProvider>
 	)
 }
 
